a9d5f0af467d248f2061e2f5738d6770baab4684
remydegrave <bolieve.fr@gmail.com>
2020-11-04 10:19:25 +0100

    exercice1pt2JS

---
+++ assets/js/fichierscript.js
@@ -0,0 +1,10 @@
+//mouseover & mouseout events// 
+function setNewImage()
+{
+    document.getElementById("img1").src ="assets/image/wm21.jpg";
+}
+function setOldImage()
+{
+    document.getElementById("img1").src ="assets/image/wm37.jpg";
+    //alert();
+}
